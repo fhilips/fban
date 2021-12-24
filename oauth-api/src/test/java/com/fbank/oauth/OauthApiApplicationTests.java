57c9d401e962ea6c3ec522306143ccc682be7a84
fhilips <fhilips.souza@gmail.com>
2021-12-24 18:34:57 -0300

    firt commit. Oauth api initicial project

---
+++ oauth-api/src/test/java/com/fbank/oauth/OauthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fbank.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
